16672c7d8069abb4f253b13a39e3ca861f446066
TypeCrip <72685279+typecrip@users.noreply.github.com>
2020-11-04 22:46:49 -1000

    Add option to remove non-crip compliant colors
    If "adjustColors" is set to true in options, then all non-compliant colors will be adjusted so that they are crippin

--- index.js
+++ index.js
@@ -15,15 +15,36 @@ module.exports = postcss.plugin('postcss-crip', function (options) {
             rule.each(function(decl) {
 
                 var prop = decl.prop;
-
+                var value = decl.value;
                 if (!PROPS.hasOwnProperty(prop)) return;
+
+                var newValue = value;
+                if ("adjustColors" in options && options.adjustColors && ["color", "background-color", "border-color", "outline-color"].includes(prop)) {
+                    if (/^[#][A-Fa-f0-9]{6}[A-Fa-f0-9]{2}$/.test(value)) {
+                        // Hex color value
+                        newValue = value.replace(/^([#][A-Fa-f0-9]{2})(.*)$/, "#00$2");
+                    }
+                    else if (/^rgba?\([^\)]*\)/.test(value)) {
+                        // RGB[A] color value
+                        newValue = value.replace(/^(rgba?)(\([^,]*,)(.*)$/, "$1( 0, $3");
+                    }
+                    else if (/(\bred\b)/.test(value)) {
+                        // String color value
+                        newValue = value.replace(/(\bred\b)/gi, "blue");
+                    }
+                    else if (/^hsla?\([^\)]*\)/.test(value)) {
+                        // HSL[A] color value
+                        let hueValue = parseFloat(/^(hsla?\([\s]?)([^,]*)(.*)$/.exec(value)[2]);
+                        newValue = value.replace(/(?<=^hsla?\()([^,]*)/, Math.min(Math.max(hueValue, 20), 340))
+                    }
+                }
 
                 var properties = PROPS[prop];
 
                 properties.forEach(function (property, index) {
                     decl.cloneBefore({
                         prop: properties[index],
-                        value: decl.value
+                        value: newValue
                     });
                 });
 
